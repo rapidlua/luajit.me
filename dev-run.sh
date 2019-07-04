bf0e50ca27a189bc256e2eafc19dd667d7444a7d
Nick Zavaritsky <mejedi@gmail.com>
2019-07-04 11:25:50 +0000

    Add script for starting the instance in dev mode

---
+++ dev-run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+set -euxo pipefail
+docker build  --iidfile .dockerid . 
+docker run \
+    --init --rm -p8000:8000 \
+    --volume $(realpath server/public):/usr/src/luajit.me/server/public \
+    --security-opt seccomp=unconfined \
+    --security-opt apparmor:unconfined \
+    $(cat .dockerid)
