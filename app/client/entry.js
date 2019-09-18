3bd71bba93356570d582170d6dc4f082018493ca
Nick Zavaritsky <mejedi@gmail.com>
2019-09-18 16:25:09 +0000

    FuncProtoDetailPanel refactoring

--- app/client/entry.js
+++ app/client/entry.js
@@ -11,6 +11,7 @@ import {CodeView} from "./codeView.js";
 import {ToggleButton} from "./ToggleButton.js";
 import {ModeSwitcher} from "./ModeSwitcher.js";
 import {TraceDetailPanel} from "./TraceDetailPanel.js";
+import {FuncProtoDetailPanel} from "./FuncProtoDetailPanel.js";
 import {number4} from "./number4.js";
 
 import "./styles.css";
@@ -212,80 +213,6 @@ class PrimaryPanel extends React.Component {
         content={content}
         contentOnClick={selectItem}
         placeholder=" "
-      />
-    );
-  }
-}
-
-function formatBool(v) {
-  if (v == true) return "Yes";
-  if (v == false) return "No";
-}
-
-/* {const} -> presentation suitable for codeView */
-function kToCodeLine(k, i) {
-  return {
-    key: i,
-    lineno: i,
-    code: k.value,
-    codeHi: k.valueHi
-  }
-}
-
-class FuncProtoDetailPanel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.infoSchema = [
-      {key:"params",     label:"Params"},
-      {key:"isvararg",   label:"Is Vararg",    fmt:formatBool},
-      {key:"stackslots", label:"Stack Slots"},
-      {key:"upvalues",   label:"Upvalues"},
-      {key:"bytecodes",  label:"Bytecodes"},
-      {key:"nconsts",    label:"Consts"},
-      {key:"gcconsts",   label:"GC Consts"},
-      {key:"children",   label:"Has Children", fmt:formatBool}
-    ];
-  }
-  render() {
-    var content;
-    if (this.props.mode == 'info') {
-      content = (
-        <PropListView
-          data={this.props.data.info}
-          schema={this.infoSchema}
-        />
-      );
-    } else {
-      content = [];
-      var proto = this.props.data;
-      if (proto.consts.length != 0) {
-        content.push(
-          <CodeView
-            key='consts'
-            className="xcode-view consts"
-            data={proto.consts.map(kToCodeLine)}
-          />
-        );
-      }
-      if (proto.gcConsts.length != 0) {
-        content.push(
-          <CodeView
-            key='gcConsts'
-            className="xcode-view consts"
-            data={proto.gcConsts.map(kToCodeLine)}
-          />
-        );
-      }
-    }
-    return (
-      <AppPanel
-        className="right-pane"
-        content={content}
-        toolbar={this.props.toolbar}
-        placeholder="No Consts"
-        panelWidth={this.props.panelWidth}
-        setPanelWidth={this.props.setPanelWidth}
       />
     );
   }
