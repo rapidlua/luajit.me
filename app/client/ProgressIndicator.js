84cd70722a99d80cce07d6171808c14ca0710b69
Nick Zavaritsky <mejedi@gmail.com>
2019-10-06 20:44:40 +0000

    Progress indicator bugfix and cleanup
    BUG: Indicator was sometimes stuck in "running" state, typically
    triggered on initial page load.

--- app/client/ProgressIndicator.js
+++ app/client/ProgressIndicator.js
@@ -1,44 +1,37 @@
-import React from 'react';
-import './ProgressIndicator.css';
+import React from "react";
+import "./ProgressIndicator.css";
 
 export class ProgressIndicator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.transitionToRunning = this.transitionToState.bind(this, 'running');
-    this.transitionToHidden = this.transitionToState.bind(this, 'hidden');
-    this.state = { indicatorState: 'hidden', activity: null };
-  }
+  state = { indicatorState: "hidden", activity: null };
   static getDerivedStateFromProps(props, state) {
     const activity = props.activity;
     return {
       activity,
       indicatorState: activity === state.activity ? state.indicatorState :
-        (activity ? 'initial' : 'finishing')
+        (activity ? "starting" : "finishing")
     };
   }
   render() {
-    let c = [''];
-    switch (this.state.indicatorState) {
-    case 'hidden': break;
-    default:       c.push('-'+this.state.indicatorState);
-    }
     return (
-      <div className={c.map(ci=>'app-progress-indicator'+ci).join(' ')}/>
+      <div className={"progress-indicator " + this.state.indicatorState}/>
     );
   }
-  transitionToState(indicatorState) {
-    const stateExpected = this.state;
-    this.setState(state => state===stateExpected ?
-      Object.assign({}, state, { indicatorState }) : state
-    );
+  transition(indicatorState) {
+    const expected = this.state.indicatorState;
+    return () => {
+      this.setState((state) =>
+        state.indicatorState !== expected ? state
+        : Object.assign({}, state, { indicatorState })
+      )
+    };
   }
   componentDidUpdate() {
     switch (this.state.indicatorState) {
-    case 'initial':
-      requestAnimationFrame(this.transitionToRunning);
+    case "starting":
+      requestAnimationFrame(this.transition("running"));
       break;
-    case 'finishing':
-      setTimeout(this.transitionToHidden, 150);
+    case "finishing":
+      setTimeout(this.transition("hidden"), 150);
       break;
     }
   }
