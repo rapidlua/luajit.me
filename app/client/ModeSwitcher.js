b8d0aee0c8a3f3db787fac755b02ef46beaa0c19
Nick Zavaritsky <mejedi@gmail.com>
2019-10-06 20:47:37 +0000

    Replace tabs with spaces

--- app/client/ModeSwitcher.js
+++ app/client/ModeSwitcher.js
@@ -1,18 +1,18 @@
 import {ToggleButton} from "./ToggleButton.js";
 
 export function ModeSwitcher(props) {
-	const currentMode = props.currentMode;
-	const selectMode = props.selectMode;
-	return (
-		<div className="toolbar-group toolbar-em">{
-			props.modes.map((mode) => (
-				<ToggleButton
-					key     = {mode.key}
-					isOn    = {currentMode == mode.key}
-					onClick = {(e)=>selectMode(e, mode.key)}
-					label   = {mode.label || mode.name}
-				/>
-			))
-		}</div>
-	);
+  const currentMode = props.currentMode;
+  const selectMode = props.selectMode;
+  return (
+    <div className="toolbar-group toolbar-em">{
+      props.modes.map((mode) => (
+        <ToggleButton
+          key     = {mode.key}
+          isOn    = {currentMode == mode.key}
+          onClick = {(e)=>selectMode(e, mode.key)}
+          label   = {mode.label || mode.name}
+        />
+      ))
+    }</div>
+  );
 }
--- app/client/ToggleButton.js
+++ app/client/ToggleButton.js
@@ -2,12 +2,12 @@ import "./ToolbarButton.css";
 import "./ToggleButton.css"
 
 export function ToggleButton(props) {
-	return (
-		<span
-			className={"toolbar-btn toolbar-sw-" + (props.isOn ? "on" : "off")}
-			onClick={props.onClick}
-		>{
-			props.label
-		}</span>
-	);
+  return (
+    <span
+      className={"toolbar-btn toolbar-sw-" + (props.isOn ? "on" : "off")}
+      onClick={props.onClick}
+    >{
+      props.label
+    }</span>
+  );
 }
