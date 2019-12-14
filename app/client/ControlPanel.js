34671108c616e5d7b5e99f5e07bcc8563e1211c2
Nick Zavaritsky <mejedi@gmail.com>
2019-12-14 22:10:39 +0000

    Edit button and dropdown menu

--- app/client/ControlPanel.js
+++ app/client/ControlPanel.js
@@ -3,6 +3,10 @@ import {PaneDivider} from "./PaneDivider.js";
 import {InspectorToolbar} from "./InspectorPanel.js";
 import {CmdButton} from "./ToolbarButton.js";
 import {Icon} from "./Icon.js";
+import {
+  Button, UncontrolledButtonDropdown, DropdownMenu,
+  DropdownItem, DropdownToggle
+} from "reactstrap";
 
 import * as Action from "./Action.js";
 
@@ -40,7 +44,7 @@ function ToolbarArea(props) {
     <div className="toolbar-area">
       <div className="toolbar primary">
         <div className="toolbar-group left">
-          <CmdButton>Edit</CmdButton>
+          <EditButton {...props}/>
           <CmdButton><Icon id="share"/> Share</CmdButton>
           <CmdButton>About</CmdButton>
         </div>
@@ -53,4 +57,47 @@ function ToolbarArea(props) {
       />
     </div>
   );
+}
+
+class EditButton extends React.PureComponent {
+  spawnEditor = () => this.props.dispatch(
+    Action.propertySet({ _showEditorOverlay: true })
+  );
+  doRefresh = () => this.props.dispatch(
+    Action.inputPropertySet({})
+  );
+  toggleInlineEditor = () => this.props.dispatch(
+    Action.paneVisibilityToggle("root.paneLayout", "inlineEditor")
+  );
+  togglePresentationMode = () => this.props.dispatch(
+    Action.propertyToggle("root.presentationMode")
+  );
+  render() {
+    const layout = this.props.state["root.paneLayout"];
+    const presentationMode = this.props.state["root.presentationMode"];
+    return (
+      <UncontrolledButtonDropdown>
+        <Button id="caret" color="primary" onClick={this.spawnEditor}>Edit</Button>
+        <DropdownToggle caret color="primary"/>
+        <DropdownMenu>
+          <DropdownItem onClick={this.doRefresh}>
+            <Icon id="refresh"/> Refresh
+          </DropdownItem>
+          <DropdownItem
+           onClick={this.toggleInlineEditor}
+           disabled={!layout.inlineEditorIsVisible && !layout._inlineEditorCanShow}
+          >
+            <Icon id="blank"/> {
+              layout.inlineEditorIsVisible ? "Hide inline editor" : "Show inline editor"
+            }
+          </DropdownItem>
+          <DropdownItem onClick={this.togglePresentationMode}>
+            <Icon id="blank"/> {
+              presentationMode ? "Leave presentation mode" : "Enter presentation mode"
+            }
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledButtonDropdown>
+    );
+  }
 }
